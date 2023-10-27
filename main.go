2e5a5f23aa0c8633b6a19a4f6135fcda0db94ba2
ferripradana <ferri.pradana@gmail.com>
2023-10-27 16:32:55 +0700

    [feat] -- tidy go mod test connection db

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fiber-gorm-microservice/infrastructure/repository/config"
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
+
+	var err error
+	_, err = config.GormOpen()
+	if err != nil {
+		fmt.Errorf("fatal error in database file: #{err}")
+	}
+
 	app := fiber.New()
 	app.Get("/", func(ctx *fiber.Ctx) error {
 		return ctx.SendString("Hello World")
